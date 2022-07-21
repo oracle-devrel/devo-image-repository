9ffc068a2a9d49f5181074b00398b8e9bb2c183f
Carlos Pano <carlos.pano@oracle.com>
2022-07-21 12:52:20 -0500

    Update AdjustHeight.js

--- public/js/AdjustHeight.js
+++ public/js/AdjustHeight.js
@@ -3,9 +3,13 @@ window.onresize = window.onload = function () {
 
     let tabContent = document.querySelectorAll(".tabs-content");
 
+    let tabs = document.getElementById("a4191c0e-0f8f-42ea-b925-194aaa836030");
+
     for (let i = 0; i < tabContent.length; i++) {
         console.log(tabContent[i].clientHeight);
       }
+
+      tabs.style.marginBottom = "1000rem";
 
 
 
