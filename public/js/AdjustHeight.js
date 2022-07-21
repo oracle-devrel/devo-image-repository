29806ef00c31bb1b06862f88a0ac86bf0795156d
Carlos Pano <carlos.pano@oracle.com>
2022-07-21 13:28:20 -0500

    Update AdjustHeight.js

--- public/js/AdjustHeight.js
+++ public/js/AdjustHeight.js
@@ -1,29 +1,17 @@
 window.onresize = window.onload = function () {
   width = this.innerWidth;
 
+  console.log("Window Widht: " + width);
+
   let tabContent = document.querySelectorAll(".tabs-content");
-
   let tabs = document.getElementById("a4191c0e-0f8f-42ea-b925-194aaa836030");
-
   var heightList = [];
 
-  for (let i = 0; i < tabContent.length; i++) {
-    heightList.push(tabContent[i].clientHeight);
+  if (window.innerWidth <= 768) {
+    for (let i = 0; i < tabContent.length; i++) {
+      heightList.push(tabContent[i].clientHeight);
+    }
+    var maxHeight = Math.max(...heightList);
+    tabs.style.marginBottom = maxHeight + "px";
   }
-
-  var maxHeight = Math.max(...heightList);
-
-  tabs.style.marginBottom = maxHeight + "px";
-
-  /*
-    mobileItem.forEach(function (item) {
-      if (window.innerWidth <= 1135) {
-        closeSearchBar();
-        hideItemNavBar(item);
-      } else {
-        closeSearchBar();
-        showItemNavBar(item);
-      }
-    });
-    */
 };
