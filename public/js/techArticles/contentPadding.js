4bc6fec95db597b3759b468858f3f32c26b3f6a4
Carlos Pano <carlos.pano@oracle.com>
2022-07-22 11:34:38 -0500

    Update contentPadding.js

--- public/js/techArticles/contentPadding.js
+++ public/js/techArticles/contentPadding.js
@@ -1,11 +1,11 @@
-window.onload = (event) => {
+window.onload = function () {
   var article = document.querySelector(".technical-content");
   var hero = document.querySelector(".content-container");
   var topicPresent = article.classList.contains("noTopics");
 
   console.log(topicPresent);
 
-  if (topicPresent == "true") {
+  if ((topicPresent = "true")) {
     hero.style.width = "66rem";
   } else {
     hero.style.width = "100rem";
