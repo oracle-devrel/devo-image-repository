216ef6482c7600ae5544d725828571c437224a04
Carlos Pano <carlos.pano@oracle.com>
2022-07-22 11:56:08 -0500

    Update contentPadding.js

--- public/js/techArticles/contentPadding.js
+++ public/js/techArticles/contentPadding.js
@@ -5,11 +5,12 @@ window.onload = function () {
 
   console.log(topicPresent);
 
-  if (topicPresent == "true") {
+  if (topicPresent = "true") {
     console.log("New Width: 66rem");
     hero.style.width = "66rem";
   } else {
     console.log("New Width: 100rem");
     hero.style.width = "100rem";
   }
+
 };
