236b2602f025281bc63c04004d8322658a59cd75
Carlos Pano <carlos.pano@oracle.com>
2022-07-22 11:19:58 -0500

    Create contentPadding.js

---
+++ public/js/techArticles/contentPadding.js
@@ -0,0 +1,11 @@
+var article = document.querySelector(".technical-content");
+var hero = document.querySelector(".content-container");
+var topicPresent = article.classList.contains("noTopics");
+
+console.log(topicPresent);
+
+if(topicPresent == "true"){
+    hero.style.width = "66rem"
+}else{
+    hero.style.width = "100rem"
+}
