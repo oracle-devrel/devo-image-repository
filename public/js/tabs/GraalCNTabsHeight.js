197de452ce55e829dde77b0ab5db5091516158fd
Carlos Pano <carlos.pano@oracle.com>
2022-07-25 17:41:33 -0500

    Update GraalCNTabsHeight.js

--- public/js/tabs/GraalCNTabsHeight.js
+++ public/js/tabs/GraalCNTabsHeight.js
@@ -16,4 +16,21 @@ window.onresize = window.onload = function () {
   } else {
     tabs.style.marginBottom = "auto";
   }
+
+  //console.log("Window Width: " + window.innerWidth);
+  var title = document.querySelectorAll(".section-header");
+
+  if (window.innerWidth <= 767) {
+    for (let i = 1; i < title.length; i++) {
+      //console.log(title[i]);
+      title[i].style.paddingTop = "0rem";
+    }
+    console.log("Mobile Headers updated");
+  } else {
+    for (let i = 1; i < title.length; i++) {
+      //console.log(title[i]);
+      title[i].style.paddingTop = "5rem";
+    }
+    console.log("Desktop Headers updated");
+  }
 };
