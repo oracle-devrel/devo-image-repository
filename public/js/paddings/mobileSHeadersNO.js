bc49671068718c74208304eaa1eae59dfde73611
Carlos Pano <carlos.pano@oracle.com>
2022-07-25 16:36:09 -0500

    New Padding

--- public/js/paddings/mobileSHeadersNO.js
+++ public/js/paddings/mobileSHeadersNO.js
@@ -3,11 +3,11 @@ window.onresize = window.onload = function () {
   var title = document.querySelectorAll(".section-header");
 
   if (window.innerWidth <= 767) {
-    for (let i = 1; i < title.length; i++) {
+    for (let i = 0; i < title.length; i++) {
       //console.log(title[i]);
       title[i].style.paddingTop = "0rem";
     }
   } else {
-    title[i].style.paddingTop = "auto";
+    title[i].style.paddingTop = "5rem";
   }
 };
