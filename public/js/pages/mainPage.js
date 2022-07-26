f20cb6dca07e1908ab3a397f77379c5fb088879e
Carlos Pano <carlos.pano@oracle.com>
2022-07-26 16:30:39 -0500

    Create mainPage.js

---
+++ public/js/pages/mainPage.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  let altImage = document.querySelectorAll("img");
+
+  for (var i = 0; i < altImage.length; i++) {
+    var image = altImage[i].getAttribute("alt");
+    //console.log(image)
+
+    if (image == null) {
+      altImage[i].setAttribute("alt", " ");
+      console.log(image + " ALT updated");
+    }
+  }
+};
