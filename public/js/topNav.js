a1557ec98d5d80014974e2543c6b20ff0ee38ee2
Carlos Pano <carlos.pano@oracle.com>
2022-07-27 16:21:11 -0500

    Updates

--- public/js/topNav.js
+++ public/js/topNav.js
@@ -19,3 +19,25 @@ function stickNavBar(x) {
 var x = window.matchMedia("(max-width: 1999px)");
 stickNavBar(x);
 x.addListener(stickNavBar);
+
+
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector(".chapters");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
