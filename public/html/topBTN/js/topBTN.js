acfb4534b18ac973ad21fbcf73c8297bfc2bd76a
Carlos Pano <carlos.pano@oracle.com>
2022-07-28 17:39:44 -0500

    Top

---
+++ public/html/topBTN/js/topBTN.js
@@ -0,0 +1,22 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
