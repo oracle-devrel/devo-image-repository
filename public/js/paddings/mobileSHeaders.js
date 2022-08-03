5f5dfbc32f5923fc3f2019f2ef9eaa1960a44965
Carlos Pano <carlos.pano@oracle.com>
2022-08-03 17:04:00 -0500

    Update

--- public/js/paddings/mobileSHeaders.js
+++ public/js/paddings/mobileSHeaders.js
@@ -33,4 +33,42 @@ window.onresize = window.onload = function () {
   let ariaSocialAnkle = document.querySelector("#social-ankle-title");
   ariaSocialAnkle.removeAttribute("aria-label");
   console.log("Aria Label for Social Ankle Removed");
+
+  //For Tutorials page:
+  let url = location.href;
+
+  if (url.includes("/dev-tutorials.html") === true) {
+    devTutorialsLinks = document.querySelectorAll(".col-section a");
+
+    for (var i = 0; i < devTutorialsLinks.length; i++) {
+      //console.log(devTutorialsLinks[i]);
+      if (devTutorialsLinks[i].href.includes("https://developer.oracle.com")) {
+        //console.log('DEVO origin included');
+
+        let generateLink = devTutorialsLinks[i].href.replace(
+          "https://developer.oracle.com/tutorials/",
+          "/site/devo/tutorials/"
+        );
+
+        let newLink = generateLink.slice(0, -1);
+
+        if (newLink.includes("/steampipe/")) {
+          devTutorialsLinks[i].href = newLink.replace("/steampipe", "");
+        } else if (newLink.includes("/oci-iac-framework/")) {
+          devTutorialsLinks[i].href = newLink.replace("/oci-iac-framework", "");
+        } else if (newLink.includes("/tf-101/")) {
+          devTutorialsLinks[i].href = newLink.replace("/tf-101", "");
+        } else if (newLink.includes("/multi-cluster-verrazzano-oke/")) {
+          devTutorialsLinks[i].href = newLink.replace(
+            "/multi-cluster-verrazzano-oke",
+            ""
+          );
+        } else {
+          devTutorialsLinks[i].href = newLink;
+        }
+        //console.log(devTutorialsLinks[i]);
+      }
+    }
+    console.log("Dev.oracle.com links removed");
+  }
 };
