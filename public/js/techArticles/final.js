fc032a9b8d459372de71e32489b8fa36c2ea3e78
Carlos Pano <carlos.pano@oracle.com>
2022-09-07 12:35:38 -0500

    Update final.js

--- public/js/techArticles/final.js
+++ public/js/techArticles/final.js
@@ -82,13 +82,6 @@ function showMT() {
       "<div class='machineTranslated' id='MachineTranslated' style='z-index: 900; top: 100px;' data-pageid='160'><div class='alert machine-translation-disclaimer' data-trackas='alert'><div class='content-wrapper'><div class='text-content'><span class='text'>此页面内容为机器翻译。</span></div><button type='button' id='closeMTalert' title='Close Alert' class='close'><svg width='20' height='20' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L11 11M21 21L11 11M11 11L21 1M11 11L1 21' stroke='white' stroke-width='2'></path></svg></button></div></div></div>"
     );
     //freeBTN.href = "https://www.oracle.com/zh/cloud/free/";
-  } else if (url.includes("/zh-TW/") === true) {
-    //Add Alert
-    topNav.insertAdjacentHTML(
-      "afterend",
-      "<div class='machineTranslated' id='MachineTranslated' style='z-index: 900; top: 100px;' data-pageid='160'><div class='alert machine-translation-disclaimer' data-trackas='alert'><div class='content-wrapper'><div class='text-content'><span class='text'>此页面内容为机器翻译。</span></div><button type='button' id='closeMTalert' title='Close Alert' class='close'><svg width='20' height='20' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L11 11M21 21L11 11M11 11L21 1M11 11L1 21' stroke='white' stroke-width='2'></path></svg></button></div></div></div>"
-    );
-    //freeBTN.href = "https://www.oracle.com/zh/cloud/free/";
   }
 
   let MTbtn = document.getElementById("closeMTalert");
@@ -302,18 +295,19 @@ window.onload = function () {
   }
 
   async function reloadDEVO() {
-    await sleep(5000);
+    console.log("Will reload...");
+    await sleep(3000);
+    window.location.reload();
   }
 
-  let retryPadding = true;
-  while (retryPadding === true) {
-    try {
-      addPadding();
-      retryPadding = false;
-      console.log("Padding added");
-    } catch (e) {
-      console.log(e);
-    }
+
+  try {
+    addPadding();
+    console.log("Padding added");
+  } catch (e) {
+    console.log(e);
+    reloadDEVO();
+    //window.location.reload();
   }
 
   try {
@@ -362,15 +356,12 @@ window.onload = function () {
   let url = location.href;
 
   if (url.includes("/en/") === false) {
-    let retryBanner = true;
-    while (retryBanner === true) {
-      try {
-        changeBanner();
-        retryBanner = false;
-        console.log("Banner changed");
-      } catch (e) {
-        console.log(e);
-      }
+    try {
+      changeBanner();
+      console.log("Banner changed");
+    } catch (e) {
+      console.log(e);
+      reloadDEVO();
     }
   }
 };
