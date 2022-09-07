5623dd1f7a770eeade418837c924b4d8a0d27b8d
Carlos Pano <carlos.pano@oracle.com>
2022-09-07 13:51:42 -0500

    Update final.js

--- public/js/techArticles/final.js
+++ public/js/techArticles/final.js
@@ -38,25 +38,25 @@ function showMT() {
       "afterend",
       "<div class='machineTranslated' id='MachineTranslated' style='z-index: 900; top: 100px;' data-pageid='160'><div class='alert machine-translation-disclaimer' data-trackas='alert'><div class='content-wrapper'><div class='text-content'><span class='text'>Diese Seite wurde maschinell übersetzt.</span></div><button type='button' id='closeMTalert' title='Close Alert' class='close'><svg width='20' height='20' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L11 11M21 21L11 11M11 11L21 1M11 11L1 21' stroke='white' stroke-width='2'></path></svg></button></div></div></div>"
     );
-    //freeBTN.href = "https://www.oracle.com/de/cloud/free/";
+    freeBTN.href = "https://www.oracle.com/de/cloud/free/";
   } else if (url.includes("/es/") === true) {
     topNav.insertAdjacentHTML(
       "afterend",
       "<div class='machineTranslated' id='MachineTranslated' style='z-index: 900; top: 100px;' data-pageid='160'><div class='alert machine-translation-disclaimer' data-trackas='alert'><div class='content-wrapper'><div class='text-content'><span class='text'>Esta página ha sido traducida por una máquina.</span></div><button type='button' id='closeMTalert' title='Close Alert' class='close'><svg width='20' height='20' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L11 11M21 21L11 11M11 11L21 1M11 11L1 21' stroke='white' stroke-width='2'></path></svg></button></div></div></div>"
     );
-    //freeBTN.href = "https://www.oracle.com/es/cloud/free/";
+    freeBTN.href = "https://www.oracle.com/es/cloud/free/";
   } else if (url.includes("/fr/") === true) {
     topNav.insertAdjacentHTML(
       "afterend",
       "<div class='machineTranslated' id='MachineTranslated' style='z-index: 900; top: 100px;' data-pageid='160'><div class='alert machine-translation-disclaimer' data-trackas='alert'><div class='content-wrapper'><div class='text-content'><span class='text'>Cette page a été traduite automatiquement.</span></div><button type='button' id='closeMTalert' title='Close Alert' class='close'><svg width='20' height='20' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L11 11M21 21L11 11M11 11L21 1M11 11L1 21' stroke='white' stroke-width='2'></path></svg></button></div></div></div>"
     );
-    //freeBTN.href = "https://www.oracle.com/fr/cloud/free/";
+    freeBTN.href = "https://www.oracle.com/fr/cloud/free/";
   } else if (url.includes("/it/") === true) {
     topNav.insertAdjacentHTML(
       "afterend",
       "<div class='machineTranslated' id='MachineTranslated' style='z-index: 900; top: 100px;' data-pageid='160'><div class='alert machine-translation-disclaimer' data-trackas='alert'><div class='content-wrapper'><div class='text-content'><span class='text'>Questa pagina è stata tradotta da una macchina.</span></div><button type='button' id='closeMTalert' title='Close Alert' class='close'><svg width='20' height='20' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L11 11M21 21L11 11M11 11L21 1M11 11L1 21' stroke='white' stroke-width='2'></path></svg></button></div></div></div>"
     );
-    //freeBTN.href = "https://www.oracle.com/it/cloud/free/";
+    freeBTN.href = "https://www.oracle.com/it/cloud/free/";
   } else if (url.includes("/ja/") === true) {
     topNav.insertAdjacentHTML(
       "afterend",
@@ -75,7 +75,7 @@ function showMT() {
       "<div class='machineTranslated' id='MachineTranslated' style='z-index: 900; top: 100px;' data-pageid='160'><div class='alert machine-translation-disclaimer' data-trackas='alert'><div class='content-wrapper'><div class='text-content'><span class='text'>Esta página foi traduzida automaticamente.</span></div><button type='button' id='closeMTalert' title='Close Alert' class='close'><svg width='20' height='20' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L11 11M21 21L11 11M11 11L21 1M11 11L1 21' stroke='white' stroke-width='2'></path></svg></button></div></div></div>"
     );
     //freeBTN.href = "https://www.oracle.com/pt-BR/cloud/free/";
-  } else if (url.includes("/zh/") === true) {
+  } else if (url.includes("/zh/") === true || url.includes("/zh-TW/") === true) {
     //Add Alert
     topNav.insertAdjacentHTML(
       "afterend",
