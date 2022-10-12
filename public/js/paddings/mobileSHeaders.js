b36d542854f26c03e3cfb073094218912768ecd2
Carlos Pano <carlos.pano@oracle.com>
2022-10-12 14:01:46 -0500

    Update

--- public/js/paddings/mobileSHeaders.js
+++ public/js/paddings/mobileSHeaders.js
@@ -1,3 +1,37 @@
+function generateNewLink(prefix, link, location) {
+  let generateLink = link.href.replace(location, prefix);
+  link.href = generateLink;
+}
+
+function changeHeaderLink() {
+  //let headerLinks = document.querySelectorAll("#Header a, #Footer a");
+  let headerLinks = document.querySelectorAll("#Header a");
+  let domain = "https://orasites-prodapp.cec.ocp.oraclecloud.com";
+  let url = location.href;
+
+  for (var i = 0; i < headerLinks.length; i++) {
+    if (headerLinks[i].href.includes(domain)) {
+      if (url.includes("/de/") === true) {
+        generateNewLink("/de", headerLinks[i], domain);
+      } else if (url.includes("/es/") === true) {
+        generateNewLink("/es", headerLinks[i], domain);
+      } else if (url.includes("/fr/") === true) {
+        generateNewLink("/fr", headerLinks[i], domain);
+      } else if (url.includes("/it/") === true) {
+        generateNewLink("/it", headerLinks[i], domain);
+      } else if (url.includes("/ja/") === true) {
+        generateNewLink("/ja", headerLinks[i], domain);
+      } else if (url.includes("/ko/") === true) {
+        generateNewLink("/ko", headerLinks[i], domain);
+      } else if (url.includes("/pt-BR/") === true) {
+        generateNewLink("/pt-BR", headerLinks[i], domain);
+      } else if (url.includes("/zh/") === true) {
+        generateNewLink("/zh", headerLinks[i], domain);
+      }
+    }
+  }
+}
+
 window.onresize = window.onload = function () {
   var title = document.querySelectorAll(".section-header");
 
@@ -11,37 +45,11 @@ window.onresize = window.onload = function () {
     }
   }
 
-  //For Tutorials page: Parse Links to work JUST ON PREVIEW
-  let url = location.href;
-
-  if (url.includes("/dev-tutorials.html") === true) {
-    devTutorialsLinks = document.querySelectorAll(".col-section a");
-
-    for (var i = 0; i < devTutorialsLinks.length; i++) {
-      if (devTutorialsLinks[i].href.includes("https://developer.oracle.com")) {
-        let generateLink = devTutorialsLinks[i].href.replace(
-          "https://developer.oracle.com/tutorials/",
-          "/site/devo/tutorials/"
-        );
-
-        let newLink = generateLink.slice(0, -1);
-
-        if (newLink.includes("/steampipe/")) {
-          devTutorialsLinks[i].href = newLink.replace("/steampipe", "");
-        } else if (newLink.includes("/oci-iac-framework/")) {
-          devTutorialsLinks[i].href = newLink.replace("/oci-iac-framework", "");
-        } else if (newLink.includes("/tf-101/")) {
-          devTutorialsLinks[i].href = newLink.replace("/tf-101", "");
-        } else if (newLink.includes("/multi-cluster-verrazzano-oke/")) {
-          devTutorialsLinks[i].href = newLink.replace(
-            "/multi-cluster-verrazzano-oke",
-            ""
-          );
-        } else {
-          devTutorialsLinks[i].href = newLink;
-        }
-      }
-    }
+  try {
+    changeHeaderLink();
+  } catch (e) {
+    console.error(e);
+    console.log("Links not updated");
   }
 };
 
@@ -74,6 +82,39 @@ window.onresize = window.onload = function () {
     if (image == null) {
       altImage[i].setAttribute("alt", "Developer Resource Center Icon");
       //console.log(image + " ALT updated");
+    }
+  }
+
+   //For Tutorials page: Parse Links to work JUST ON PREVIEW
+  let url = location.href;
+
+  if (url.includes("/dev-tutorials.html") === true) {
+    devTutorialsLinks = document.querySelectorAll(".col-section a");
+
+    for (var i = 0; i < devTutorialsLinks.length; i++) {
+      if (devTutorialsLinks[i].href.includes("https://developer.oracle.com")) {
+        let generateLink = devTutorialsLinks[i].href.replace(
+          "https://developer.oracle.com/tutorials/",
+          "/site/devo/tutorials/"
+        );
+
+        let newLink = generateLink.slice(0, -1);
+
+        if (newLink.includes("/steampipe/")) {
+          devTutorialsLinks[i].href = newLink.replace("/steampipe", "");
+        } else if (newLink.includes("/oci-iac-framework/")) {
+          devTutorialsLinks[i].href = newLink.replace("/oci-iac-framework", "");
+        } else if (newLink.includes("/tf-101/")) {
+          devTutorialsLinks[i].href = newLink.replace("/tf-101", "");
+        } else if (newLink.includes("/multi-cluster-verrazzano-oke/")) {
+          devTutorialsLinks[i].href = newLink.replace(
+            "/multi-cluster-verrazzano-oke",
+            ""
+          );
+        } else {
+          devTutorialsLinks[i].href = newLink;
+        }
+      }
     }
   }
 
--- public/js/techArticles/final.js
+++ public/js/techArticles/final.js
@@ -1,3 +1,37 @@
+function generateNewLink(prefix, link, location) {
+  let generateLink = link.href.replace(location, prefix);
+  link.href = generateLink;
+}
+
+function changeHeaderLink() {
+  //let headerLinks = document.querySelectorAll("#Header a, #Footer a");
+  let headerLinks = document.querySelectorAll("#Header a");
+  let domain = "https://orasites-prodapp.cec.ocp.oraclecloud.com";
+  let url = location.href;
+
+  for (var i = 0; i < headerLinks.length; i++) {
+    if (headerLinks[i].href.includes(domain)) {
+      if (url.includes("/de/") === true) {
+        generateNewLink("/de", headerLinks[i], domain);
+      } else if (url.includes("/es/") === true) {
+        generateNewLink("/es", headerLinks[i], domain);
+      } else if (url.includes("/fr/") === true) {
+        generateNewLink("/fr", headerLinks[i], domain);
+      } else if (url.includes("/it/") === true) {
+        generateNewLink("/it", headerLinks[i], domain);
+      } else if (url.includes("/ja/") === true) {
+        generateNewLink("/ja", headerLinks[i], domain);
+      } else if (url.includes("/ko/") === true) {
+        generateNewLink("/ko", headerLinks[i], domain);
+      } else if (url.includes("/pt-BR/") === true) {
+        generateNewLink("/pt-BR", headerLinks[i], domain);
+      } else if (url.includes("/zh/") === true) {
+        generateNewLink("/zh", headerLinks[i], domain);
+      }
+    }
+  }
+}
+
 function showMT() {
   //Crosslinking to Free Tier
   //let topNav = document.getElementById("TopNav");
@@ -376,6 +410,20 @@ window.onload = function () {
   } catch (e) {
     console.log(e);
     reloadDEVO();
+  }
+
+  try {
+    changeHeaderLink();
+  } catch (e) {
+    console.error(e);
+    console.log("Links not updated");
+  }
+
+  try {
+    changeHeaderLink();
+  } catch (e) {
+    console.error(e);
+    console.log("Links not updated");
   }
 
   /*
